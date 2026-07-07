9ca95e95ca23c821d2ec566e3d70af768fc6b7e8
Blossum <aliix.caparros@gmail.com>
2026-07-07 06:33:56 +0200

    Update supabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -2,5 +2,5 @@
 // Tu les trouves dans Supabase → Project Settings → API.
 // Ce fichier est chargé AVANT suivi.js sur la page suivi.html.
 
-window.SUPABASE_URL = 'https://kexbybipgvdxkywpwfhr.supabase.co/rest/v1/';
+window.SUPABASE_URL = 'https://kexbybipgvdxkywpwfhr.supabase.co';
 window.SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtleGJ5YmlwZ3ZkeGt5d3B3ZmhyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODMzNzk3MzcsImV4cCI6MjA5ODk1NTczN30.Fpsq3EGJmU7CgZvNzmIkkC_Vf3vD_EE7MwB03L6RUwc';
